142f150affa84914fe52913c73f7eff499b049d7
kelvintrc <kvynmutwiri@gmail.com>
2022-03-10 16:58:47 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ *
+ *Return: returns 1
+ */ 
+
+int main()
+{
+	fprintf(stdout,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
