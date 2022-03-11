21cf20ae78a33ff56d66d9ba6eccf6050c4c5fc8
kelvintrc <kvynmutwiri@gmail.com>
2022-03-11 12:34:52 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - contains the code for program execution
+ *
+ *Return: returns 0 (Success), otherwise non-zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
