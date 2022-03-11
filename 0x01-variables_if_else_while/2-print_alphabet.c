7c6c56c18eef3755bf070e2e42d6a2b0bbff9b98
kelvintrc <kvynmutwiri@gmail.com>
2022-03-11 10:27:55 +0000

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,12 @@
  *main - contains the code for program execution
  *
  *Return: returns 0 (Success), otherwise non-zero
- */ 
+ */
 
 int main(void)
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
