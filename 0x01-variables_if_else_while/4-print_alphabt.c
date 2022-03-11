8476a4d6bce87231a727f0806fa8d5a8229d064a
kelvintrc <kvynmutwiri@gmail.com>
2022-03-11 11:57:03 +0000

    When i was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - contains the code for program execution
+ *
+ *Return: returns 0 (Success), otherwise non-zero
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
