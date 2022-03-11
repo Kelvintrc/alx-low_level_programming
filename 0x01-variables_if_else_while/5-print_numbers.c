40da2eea88261737fea50b8abf4b45907e5e0177
kelvintrc <kvynmutwiri@gmail.com>
2022-03-11 12:11:37 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Contains the code for program execution
+ *
+ *Return: returns 0(Success), otherwise non-zero
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
