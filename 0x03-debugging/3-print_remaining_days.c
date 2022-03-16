c2bc3551802dd694ed3df383a09d80e185527716
kelvintrc <kvynmutwiri@gmail.com>
2022-03-16 14:51:31 +0000

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,12 +22,11 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else if (month == 2 && day == 60 && (year % 4 == 0))
-                {
-                        printf("Day of the year: %d\n", day);
-                        printf("Remaining days: %d\n", 366-day);
-                }
-
+	else if ((month == 2 && day == 60) && (year % 4 == 0))
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
 	else
 	{
 		if (month == 2 && day == 60)
