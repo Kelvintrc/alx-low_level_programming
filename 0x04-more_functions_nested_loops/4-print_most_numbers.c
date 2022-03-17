9ab5a3c5653b7acad32e5f1304cfc3691477cf5f
kelvintrc <kvynmutwiri@gmail.com>
2022-03-17 15:54:18 +0000

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	char ch;
+
+	ch = '0';
+
+	while (ch <= '9')
+	{
+		if (ch != '2' && ch != '4')
+		{
+			_putchar(ch);
+		}
+
+		ch++;
+	}
+
+	_putchar('\n');
+}
