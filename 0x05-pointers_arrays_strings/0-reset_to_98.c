51906b35afe7828365dcaf188aaf40a560d3c457
kelvintrc <kvynmutwiri@gmail.com>
2022-03-21 08:33:35 +0000

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
  *
- * @*n: pointer to an int
+ * @n: pointer to an int
  */
 void reset_to_98(int *n)
 {
