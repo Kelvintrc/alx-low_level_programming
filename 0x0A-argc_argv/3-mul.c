dc3e102f4b013ee76b4a69a08a12e2b50dbe813d
kelvintrc <kvynmutwiri@gmail.com>
2022-04-01 12:47:29 +0000

    multipying two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,7 @@
 /**
  * main - multiplies two numbers
  * @argc: argument count
- * @agrv: argument vector
+ * @argv: argument vector
  * Return: 0 on success
  */
 
@@ -15,11 +15,12 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
-	
+
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
-	
+
 	return (0);
 }
