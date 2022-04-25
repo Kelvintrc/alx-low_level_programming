88aa89b1a8f159e5d2e8ac41335087b4613d35e4
kelvintrc <kvynmutwiri@gmail.com>
2022-04-25 16:03:04 +0000

    [Feature]: Returning the number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+/**
+ * listint_len - returns the number of elements in a linked
+ * listint_t list
+ * @h: head of the list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
