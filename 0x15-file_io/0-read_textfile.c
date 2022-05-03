69e8c9137c60846b3479fc46c890c979a370b28d
kelvintrc <kvynmutwiri@gmail.com>
2022-05-03 13:17:28 +0000

    [Feature]: Reading a text file and printing it to the stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and 
+ * prints it to the POSIX standard output
+ * @filename: the file to be read
+ * @letters: the number of letters it
+ * could read and print
+ *
+ * Return: Actual number of letters it could
+ * read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+# 0x15. C - File I/O
+
+		Tasks:
+
+0. Tread lightly, she is near - A function that reads a text file and prints it to the POSIX standart output, using Prototype: ssize_t read_textfile(const char *filename, size_t letters);
