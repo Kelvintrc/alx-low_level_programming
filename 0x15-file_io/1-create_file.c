96736a3cec7cd13b9f33a2a1c4a128d42c751b29
kelvintrc <kvynmutwiri@gmail.com>
2022-05-03 15:40:27 +0000

    [Feature]: A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: name of the file to create
+ * @text_content - NULL terminated strint to
+ * to write to the file name
+ *
+ * Return: 1 on success, -1 on failure
+ */  
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
