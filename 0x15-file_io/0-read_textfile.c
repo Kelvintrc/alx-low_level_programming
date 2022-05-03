178b526f90089f65ad59e61b5bc2468e2f14a61f
kelvintrc <kvynmutwiri@gmail.com>
2022-05-03 13:25:16 +0000

    [Fix]: Reading a text file and printing it to the stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and 
+ * read_textfile - reads a text file and
  * prints it to the POSIX standard output
  * @filename: the file to be read
  * @letters: the number of letters it
