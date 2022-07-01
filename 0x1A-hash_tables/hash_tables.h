b072f515ed7e537cd687adb8a28016514d4b767f
kelvintrc <kvynmutwiri@gmail.com>
2022-07-01 13:50:38 +0000

    [Feature]: A function that retrieves a value associated with a key

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -46,4 +46,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 /* 3-hash_table_set.c*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
+/* 4-hash_table_get.c */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
 #endif /* __HASH_TABLES__ */
