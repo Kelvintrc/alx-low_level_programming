6a18be2eade698b0de3ee41f9d4fa89a8f1d5352
kelvintrc <kvynmutwiri@gmail.com>
2022-07-01 13:08:51 +0000

    [Feature]: A function that gives you the index of a key

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,4 +40,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 /* 1-djb2.c */
 unsigned long int hash_djb2(const unsigned char *str);
 
+/* 2-key_index.c */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
 #endif /* __HASH_TABLES__ */
