2598889cde2cd0ac21c1102effdf7fed2d4fcdad
kelvintrc <kvynmutwiri@gmail.com>
2022-10-05 16:32:51 +0000

    [Feature]: function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,7 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+void array_print(int *array, size_t lo, size_t hi);
 
 #endif /*_SEARCH_ALGOS_H_*/
