ea435e5c6d7771f48630b54f466dddfb3024057a
kelvintrc <kvynmutwiri@gmail.com>
2022-10-05 16:47:44 +0000

    [Feature]: Advanced binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /*_SEARCH_ALGOS_H_*/
